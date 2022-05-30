f57d66cd3744c922fd2d2c446956df7aa425ecf3
jindory <laonjay@gmail.com>
2022-05-31 07:31:57 +0900

    프로미스 연결하기 예제

--- promise-exercises3.js
+++ promise-exercises3.js
@@ -21,3 +21,44 @@ getData().then(function(data){
 }).catch(function(err){
     console.err(err); //Error 출력
 })
+
+
+
+//여러개의 프로미스 연결하기
+function getData(){
+    return new Promise({
+       
+    });
+}
+
+getData()
+    .then(function(data){
+        //...
+    })
+    .then(function(){
+        //...
+    })
+    .then(fucntion(){
+        //...
+    });
+
+
+//여러개의 프로미스 연결하기 예제
+//프로미스 객체를 하나 생성하고 setTimeout()을 이용해 2초 후에 resolve()를 호출하는 예제
+new Promise((resolve, reject)=>{
+    setTimeout(function(){
+        resolve(1)
+        //resolve가 호출되면 프로미스가 대기 상태에서 이행 상태로 넘어가기 땜문에 .then()의 로직으로 넘어갑니다.
+    }, 2000);
+})
+.then(function(result){
+    console.log(result); //1
+    return result+10;
+})
+.then(function(result){ 
+    console.log(result); //11
+    return result+20;
+})
+.then(function(result){ 
+    console.log(result); //31
+});
